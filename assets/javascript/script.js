bd8d440f5f56ecbee7747269bcaa4dff7a9ad4e0
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-03-23 21:55:51 +0800

    getting query string

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,19 @@
-console.log("hello there");
+let searchFormEl = document.querySelector("#search-form");
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    let searchCityInput = document.querySelector("#search-city-input").value;
+    const APIkey = "ad132697fb09c0a3d0781a7f1977e112";
+
+    if(!searchCityInput) {
+        console.error("you need a search input value");
+        return;
+    }
+
+    let queryString = "/search-results.html?q=" + searchCityInput + "?APIID=" +APIkey;
+    console.log("query string: " + queryString);
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
