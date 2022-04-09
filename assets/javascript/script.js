fd2728b268d62115327ed2a45756fcbcafe51a6b
Bernard Sin Fat <bernardjbs@yahoo.com>
2022-04-09 22:43:23 +0800

    Added localstorage for cities searched

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,8 +7,41 @@ const iconUrl = "https://openweathermap.org/img/wn";
 
 const APIkey = "ad132697fb09c0a3d0781a7f1977e112";
 let cityArr = [];
+let defaultCity = "";
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+localStorage_CheckCities();
+
+searchHistoriesEl.addEventListener('click', (event) => {
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+      return;
+    }
+    searchLocation(event.target.id)
+    .then(Weather => {
+        renderWeather(Weather);
+        renderCurrentWeather(Weather);
+        renderForecast(Weather);
+        
+    });
+    console.dir(event.target.id);
+  })
+
+function localStorage_CheckCities() {
+    if (JSON.parse(localStorage.getItem("cities"))) {
+        cityArr = JSON.parse(localStorage.getItem("cities"));
+        defaultCity = cityArr[0];
+        console.log("default city: " + defaultCity);
+        searchLocation(defaultCity)
+            .then(Weather => {
+                renderCurrentWeather(Weather);
+                renderForecast(Weather);
+                renderCityButtons(cityArr);
+                defaultCity = "";
+            });
+    }
+}
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -60,6 +93,7 @@ async function searchLocation(query) {
     // Fill and return the Weather object
     return {
         city: jsonWeatherData.name,
+        defaultCity: defaultCity,
         date: unixToDate(currentData.dt),
         currentTemp: currentData.temp,
         currentWindSpeed: currentData.wind_speed,
@@ -70,46 +104,52 @@ async function searchLocation(query) {
     }
 }
 
-function renderWeather(Weather) {
+const renderWeather = (Weather) => {
     currentWeatherEl.innerHTML = "";
     forecastEl.innerHTML = "";
-    const cityButton = document.createElement("Button");
-    if (cityArr.includes(Weather.city)) {
+    if (cityArr.includes(Weather.city) && Weather.defaultCity != "") {
         return;
     }
     else {
         cityArr.push(`${Weather.city}`);
-        cityButton.textContent = Weather.city;
-        searchHistoriesEl.append(cityButton)
-    }
-    cityArr = [... new Set(cityArr)];
-    searchCityInput.value = "";
 
+    }
+    searchCityInput.value = "";
+    cityArr = [... new Set(cityArr)];
+    // Save cityArr to localstorage
+    localStorage.setItem("cities", JSON.stringify(cityArr));
+
+    renderCityButtons(cityArr);
     renderCurrentWeather(Weather);
     renderForecast(Weather);
 
 }
 
 // Function to convert unix timestamp to date format
-function unixToDate(unix) {
+const unixToDate = (unix) => {
     const unixDate = unix;
     const milliSecs = new Date(unixDate * 1000);
-    const day = milliSecs.getDate();
-    const month = milliSecs.getMonth() + 1;
+    let day = milliSecs.getDate();
+    let month = milliSecs.getMonth() + 1;
     const year = milliSecs.getFullYear();
+    if (day < 10) day = `0${day}`;
+    if (month < 10) month = `0${month}`;
     const dateFormat = `${day}/${month}/${year}`;
     return dateFormat;
 }
 
 // Function to render the current weather
-    const renderCurrentWeather = (Weather) => {
+const renderCurrentWeather = (Weather) => {
+    while(currentWeatherEl.firstChild) {
+        currentWeatherEl.removeChild(currentWeatherEl.firstChild);
+    }
     const cwH2 = document.createElement("h2");
     const cwIcon = document.createElement("img");
     const cwUl = document.createElement("ul");
     const cwLiTemp = document.createElement("li");
-    const cwLiWind = document.createElement("li"); 
-    const cwLiHumidity = document.createElement("li"); 
-    const cwLiUVindex = document.createElement("li"); 
+    const cwLiWind = document.createElement("li");
+    const cwLiHumidity = document.createElement("li");
+    const cwLiUVindex = document.createElement("li");
 
     // Set the text for current weather H2 element
     cwH2.textContent = `${Weather.city} - ${Weather.date}`;
@@ -137,52 +177,68 @@ function unixToDate(unix) {
 }
 
 // Function to render the forecast weather
-    const renderForecast = (Weather) => {
-    const forecastArr = Weather.forecast;
-    for(i=0; i<forecastArr.length; i++) {
-
-    // Create new elements for the forecast section
-    const cardsEl = document.createElement("section");
-    const dailyForecastEl = document.createElement("section");    
-    let datePel = document.createElement("p");
-    const iconPel = document.createElement("img");
-    const fcUl = document.createElement("ul"); 
-    const fcLiTemp = document.createElement("li"); 
-    const fcLiWind = document.createElement("li"); 
-    const fcLiHumidity = document.createElement("li"); 
-    const fcLiUVindex = document.createElement("li"); 
-
-    // Setting IDs and Classes to new elements
-    cardsEl.setAttribute("id", "cards");
-    dailyForecastEl.setAttribute("class", "daily-forecast");
-
-    // Fill the Date and Icon data
-     datePel = forecastArr[i].date;
-     iconPel.src = `${iconUrl}/${forecastArr[i].icon}@2x.png` ;
-
-    // Create text nodes for the li elements
-    fcLiTemp.appendChild(document.createTextNode(`Temperature: ${forecastArr[i].temperature}`));
-    fcLiWind.appendChild(document.createTextNode(`Wind: ${forecastArr[i].windSpeed}`));
-    fcLiHumidity.appendChild(document.createTextNode(`Humidity: ${forecastArr[i].umidity}`));
-    fcLiUVindex.appendChild(document.createTextNode(`UV Index: ${forecastArr[i].uvi}`));
-
-    // Append the li elements to the current weather ul
-    fcUl.appendChild(fcLiTemp);
-    fcUl.appendChild(fcLiWind);
-    fcUl.appendChild(fcLiHumidity);
-    fcUl.appendChild(fcLiUVindex);
-
-    // Append Weather data elements to daily-forcast section
-    dailyForecastEl.append(datePel);
-    dailyForecastEl.append(iconPel);
-    dailyForecastEl.append(fcUl);
-
-    // Append daily forecast section to cards section
-    cardsEl.append(dailyForecastEl);
-
-    // Append cards section to to forecast section
-    forecastEl.appendChild(cardsEl);
+const renderForecast = (Weather) => {
+    while(forecastEl.firstChild) {
+        forecastEl.removeChild(forecastEl.firstChild);
     }
+    const forecastArr = Weather.forecast;
+    for (i = 0; i < forecastArr.length; i++) {
 
+        // Create new elements for the forecast section
+        const cardsEl = document.createElement("section");
+        const dailyForecastEl = document.createElement("section");
+        let datePel = document.createElement("p");
+        const iconPel = document.createElement("img");
+        const fcUl = document.createElement("ul");
+        const fcLiTemp = document.createElement("li");
+        const fcLiWind = document.createElement("li");
+        const fcLiHumidity = document.createElement("li");
+        const fcLiUVindex = document.createElement("li");
+
+        // Setting IDs and Classes to new elements
+        cardsEl.setAttribute("class", "cards");
+        dailyForecastEl.setAttribute("class", "daily-forecast");
+
+        // Fill the Date and Icon data
+        datePel = forecastArr[i].date;
+        iconPel.src = `${iconUrl}/${forecastArr[i].icon}@2x.png`;
+
+        // Create text nodes for the li elements
+        fcLiTemp.appendChild(document.createTextNode(`Temperature: ${forecastArr[i].temperature}`));
+        fcLiWind.appendChild(document.createTextNode(`Wind: ${forecastArr[i].windSpeed}`));
+        fcLiHumidity.appendChild(document.createTextNode(`Humidity: ${forecastArr[i].umidity}`));
+        fcLiUVindex.appendChild(document.createTextNode(`UV Index: ${forecastArr[i].uvi}`));
+
+        // Append the li elements to the current weather ul
+        fcUl.appendChild(fcLiTemp);
+        fcUl.appendChild(fcLiWind);
+        fcUl.appendChild(fcLiHumidity);
+        fcUl.appendChild(fcLiUVindex);
+
+        // Append Weather data elements to daily-forcast section
+        dailyForecastEl.append(datePel);
+        dailyForecastEl.append(iconPel);
+        dailyForecastEl.append(fcUl);
+
+        // Append daily forecast section to cards section
+        cardsEl.append(dailyForecastEl);
+
+        // Append cards section to to forecast section
+        forecastEl.appendChild(cardsEl);
+    }
 }
-// TODO: Local storage for city buttons
+
+// Function to render 
+const renderCityButtons = (cityArr) => {
+    while(searchHistoriesEl.firstChild) {
+        searchHistoriesEl.removeChild(searchHistoriesEl.firstChild);
+    }
+    cityArr.forEach(city => {
+        const cityButton = document.createElement("Button");
+        cityButton.setAttribute("class", "btn btn-primary city-button")
+        cityButton.setAttribute("id", city);
+        cityButton.textContent = city;
+        searchHistoriesEl.append(cityButton)
+        console.log(`city: ${city}`);
+    });
+}
